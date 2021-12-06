a863c6e0ec9e6df4baf8239700a172a58b601c14
Andrew Royal <aroyal641@gmail.com>
2021-12-06 14:25:29 -0800

    Add files via upload

---
+++ s3.R
@@ -0,0 +1,25 @@
+read_s3_file = function(bucket, key, read_fun, uri_arg = 'input', ...) {
+  read_args = list(...)
+  file_uri = tempfile()
+  read_args[[uri_arg]] = file_uri
+  s3$download_file(Bucket = bucket, Key = key, Filename = file_uri)
+  do.call('read_fun', read_args)
+}
+
+write_s3_file = function(x, bucket, key, write_fun, object_arg = 'x', uri_arg = 'file', ...) {
+  write_args = list(...)
+  file_uri = tempfile()
+  write_args[[object_arg]] = x
+  write_args[[uri_arg]] = file_uri
+  do.call('write_fun', write_args)
+  
+  s3$upload_file(
+    Filename = file_uri,
+    Bucket = bucket,
+    Key = key)
+}
+
+list_contents = function(objects, filter) {
+  contents = sapply(objects$Contents, function(x) x$Key)
+  contents[grepl(filter, contents)]
+}
